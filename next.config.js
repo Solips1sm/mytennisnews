a34bd5e4574d05b40052a8c1c1cc0e85de6fe059
Anistratenco Serghei <n1keloffs@gmail.com>
2025-10-01 22:18:29 +0300

    Build fix

--- next.config.js
+++ next.config.js
@@ -15,11 +15,23 @@ const nextConfig = {
       'groq',
     ],
   },
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
     config.resolve = config.resolve ?? {}
     config.resolve.alias = config.resolve.alias ?? {}
     if (!config.resolve.alias['#async_hooks']) {
       config.resolve.alias['#async_hooks'] = 'node:async_hooks'
+    }
+    if (isServer) {
+      const externals = Array.isArray(config.externals)
+        ? config.externals
+        : config.externals
+        ? [config.externals]
+        : []
+      externals.push({
+        sleep: 'commonjs sleep',
+        'sleep/build/Release/node_sleep.node': 'commonjs sleep/build/Release/node_sleep.node',
+      })
+      config.externals = externals
     }
     return config
   },
